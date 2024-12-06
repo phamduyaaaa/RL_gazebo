5f3f9053e234c835a86946891c75e852f6af6ff1
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2024-12-06 13:51:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import yaml
 
 with open("config.yaml", "r") as file:
       config = yaml.safe_load(file)
-      # Training
       init_x = config["pos"]["init_x"]
       init_y = config["pos"]["init_y"]
       goal_x = config["pos"]["goal_x"]
       goal_y = config["pos"]["goal_y"]
-      goal_y = config["num_episodes"]
+      num_episodes = config["num_episodes"]
