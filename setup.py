2ac65ec9123aa465d3631f60f19344d6dfbc38ee
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-01-10 20:22:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ import yaml
 
 with open("config.yaml", "r") as file:
       config = yaml.safe_load(file)
+      # Training
       init_x = config["pos"]["init_x"]
       init_y = config["pos"]["init_y"]
       goal_x = config["pos"]["goal_x"]
       goal_y = config["pos"]["goal_y"]
       num_episodes = config["num_episodes"]
+      replay_buffer_size = config["replay_buffer_size"]
