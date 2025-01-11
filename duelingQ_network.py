8a4397c6659d1d9b0a4ab8ff3d6f866678067095
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-01-11 14:51:12 +0700

    Update duelingQ_network.py

--- duelingQ_network.py
+++ duelingQ_network.py
@@ -9,16 +9,18 @@ class DuelingQNetwork(nn.Module):
         self.num_quantiles = 51
 
         # Convolutional layers with Batch Normalization
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=2)  # kernel size and stride adjusted
+        # Sửa lại conv1 để xử lý dữ liệu đầu vào có 1 kênh
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(32)
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)  # kernel size and stride adjusted
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
         self.bn2 = nn.BatchNorm2d(64)
 
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.bn3 = nn.BatchNorm2d(64)
 
         # Fully connected layers
+        # Điều chỉnh kích thước của fc1 để phù hợp với kích thước đầu ra của tầng tích chập
         self.fc1 = nn.Linear(64 * 32 * 40, 512)
         self.dropout = nn.Dropout(p=0.5)  # Dropout for regularization
         self.fc2 = nn.Linear(512, self.num_actions * self.num_quantiles)
@@ -28,6 +30,7 @@ class DuelingQNetwork(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+
         # Flatten before fully connected layers
         x = x.view(x.size(0), -1)
 
